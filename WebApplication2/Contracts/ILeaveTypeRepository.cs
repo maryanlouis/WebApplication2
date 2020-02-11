0c6f987cd772ba02ec4e825fb7716092c88262db
maro emy <you@example.com>
2020-02-11 14:00:45 +0200

    Add project files.

---
+++ WebApplication2/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+
+namespace WebApplication2.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
---
+++ WebApplication2/Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveAllocation , LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
